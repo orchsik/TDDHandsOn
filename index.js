bb9921ae8021b9f786222a524e638ce4565b98b8
orchsik <orchsik@naver.com>
2022-09-11 16:10:56 +0900

    refactoring

--- index.js
+++ index.js
@@ -1,18 +1,28 @@
-refineText = (s, bannedWorlds = []) => {
-  s = s
-    .replace("   ", " ")
-    .replace("  ", " ")
-    .replace("   ", " ")
-    .replace("  ", " ")
-    .replace(" \t", " ")
-    .replace("\t ", " ");
-
-  bannedWorlds.forEach((bannedWorld) => {
-    // console.log("@@@", { bannedWorld, re: "*".repeat(bannedWorld.length) });
-    s = s.replace(bannedWorld, "*".repeat(bannedWorld.length));
-  });
-
-  return s;
+refineText = (source, bannedWorlds = []) => {
+  return [normalizeWhitespcae, compactWhiteSpcae, maskBannedWords].reduce(
+    (value, filter) => {
+      return filter(value, bannedWorlds);
+    },
+    source
+  );
 };
 
 module.exports = refineText;
+
+const compactWhiteSpcae = (source) => {
+  return source.indexOf("  ") < 0
+    ? source
+    : compactWhiteSpcae(source.replace("  ", " "));
+};
+
+function maskBannedWords(source, bannedWorlds) {
+  bannedWorlds.forEach((bannedWorld) => {
+    const mask = "*".repeat(bannedWorld.length);
+    source = source.replace(bannedWorld, mask);
+  });
+  return source;
+}
+
+function normalizeWhitespcae(source) {
+  return source.replace("\t", " ");
+}
